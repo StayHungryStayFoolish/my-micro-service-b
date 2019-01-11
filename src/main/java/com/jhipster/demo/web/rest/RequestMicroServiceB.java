7722a31b1035946d3d1b03fbade1b908da735919
StayHungryStayFoolish <bonismo@hotmail.com>
2019-01-11 18:14:28 +0800

    test @AuthorizedFeignClient(Open API Secure)

--- src/main/java/com/jhipster/demo/web/rest/RequestMicroServiceB.java
+++ src/main/java/com/jhipster/demo/web/rest/RequestMicroServiceB.java
@@ -2,18 +2,118 @@ package com.jhipster.demo.web.rest;
 
 import com.codahale.metrics.annotation.Timed;
 import com.jhipster.demo.security.AuthoritiesConstants;
+import com.jhipster.demo.security.SecurityUtils;
 import com.jhipster.demo.web.rest.vm.Person;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.authentication.OAuth2AuthenticationDetails;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.util.*;
 
 @RestController
-@RequestMapping("/api/demo")
+@RequestMapping("/api/demo/b")
 public class RequestMicroServiceB {
 
+    private final Logger log = LoggerFactory.getLogger(RequestMicroServiceB.class);
+
+    @Autowired
+    TokenStore tokenStore;
+
+    @GetMapping("/login")
+    public String getLogin(HttpServletRequest request) {
+
+        // Parse Cookie base on HttpServletRequest
+        Cookie[] cookies = request.getCookies();
+        String access_token = "";
+        if (null != cookies) {
+
+            for (Cookie cookie : cookies) {
+                if ("access_token".equalsIgnoreCase(cookie.getName())) {
+                    access_token = cookie.getValue();
+                    log.info("ACCESS_TOKEN : {} ", access_token);
+                }
+            }
+        }
+
+        // Parse Header Param base on HttpServletRequest
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            log.info("Header Key : {} - Value : {}", key, value);
+        }
+
+        String header = request.getHeader("Authorization");
+        if (StringUtils.isNoneBlank(header)) {
+            String token = StringUtils.substringAfter(header, "Bearer ");
+            log.info("Bearer token : {}" + token);
+
+            if (StringUtils.isEmpty(token)) {
+                log.error("Obtaining user information anomaly");
+            } else {
+
+                OAuth2AccessToken accessToken = tokenStore.readAccessToken(token);
+                Map<String, Object> map = accessToken.getAdditionalInformation();
+                if (null == map) {
+                    log.error("Obtaining user information anomaly");
+                }
+
+                String user_name = (String) map.get("user_name");
+                if (StringUtils.isBlank(user_name)) {
+                    log.error("Obtaining user information anomaly");
+                }
+
+                log.info("user_name : {}", user_name);
+            }
+        }
+
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication authentication = securityContext.getAuthentication();
+
+        if (authentication != null && authentication.getDetails() instanceof OAuth2AuthenticationDetails) {
+
+            OAuth2AuthenticationDetails details = (OAuth2AuthenticationDetails) authentication.getDetails();
+
+            if (null != details) {
+                log.info("TokenValue : {}", details.getTokenValue());
+            }
+
+            String authorization = request.getHeader("Authorization");
+            if (StringUtils.isNoneBlank(authorization)) {
+                log.info("Authorization : {}", authorization);
+            }
+        }
+
+        // judge the current user base on SecurityContext
+        Authentication contextAuthentication = securityContext.getAuthentication();
+        if (null != contextAuthentication && contextAuthentication.isAuthenticated()) {
+            if (contextAuthentication.getPrincipal() instanceof String) {
+
+                log.info("Principal : {} ", contextAuthentication.getPrincipal());
+
+            }
+        }
+
+        Optional<String> login = SecurityUtils.getCurrentUserLogin();
+
+        if (login.isPresent()) {
+            log.info("Login : {} ", login.get());
+            return login.get();
+        } else {
+            return "None";
+        }
+    }
 
     @GetMapping("/string-b")
     @Timed
